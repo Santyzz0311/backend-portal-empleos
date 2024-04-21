43755cc058c69d15c3ef70c456d58a135b14f503
Santyzz0311 <santyg0311@gmail.com>
2024-04-20 22:53:53 -0500

    Feat: context de conecion con la base de datos

---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using backend_portal_empleos.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_portal_empleos.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Jobs> Jobs { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Applications> Applications { get; set; }
+    }
+}
