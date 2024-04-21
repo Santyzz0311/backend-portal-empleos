a5805b1a67dd1dbae43626f5cc6101579e77ebc5
Santyzz0311 <santyg0311@gmail.com>
2024-04-20 23:00:50 -0500

    Feat: config visual studio

--- obj/Debug/net8.0/backend-portal-empleos.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend-portal-empleos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-portal-empleos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb93b27ccd8405a0d8cebe35892df24a05177a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-portal-empleos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-portal-empleos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
